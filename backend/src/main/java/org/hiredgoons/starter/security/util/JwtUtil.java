2fb514d54663cabb20b0d7afdf9954c5b7f36f57
Jason Ferguson <fergusonjason@gmail.com>
2023-02-07 05:45:26 -0500

    implement JwtUtil

---
+++ backend/src/main/java/org/hiredgoons/starter/security/util/JwtUtil.java
@@ -0,0 +1,45 @@
+package org.hiredgoons.starter.security.util;
+
+import java.time.Instant;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+
+/**
+ * Utility class for dealing with JWTs.
+ * 
+ * These methods assuming a very simply token with only one claim: "userId". If
+ * you want to validate more claims, you get to write the code to do it.
+ * 
+ * @author jason
+ *
+ */
+public class JwtUtil {
+
+	public static String generateToken(String username, String secret, Instant expiration) {
+		
+		return JWT.create()
+				.withSubject("User Details")
+				.withClaim("userId", username)
+				.withIssuedAt(Instant.now())
+				.withIssuer("me")
+				.withExpiresAt(expiration)
+				.sign(Algorithm.HMAC256(secret));
+	}
+	
+	public static String validateToken(String token, String secret) throws JWTVerificationException {
+		
+		JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
+				.withSubject("User Details")
+				.withIssuer("me")
+				.build();
+		
+		DecodedJWT jwt = verifier.verify(token);
+		
+		return jwt.getClaim("userId").asString();
+		
+	}
+}
