9277ec165747f3f81c277a733feb109ab1602bf6
Jason Ferguson <fergusonjason@gmail.com>
2023-02-07 04:11:01 -0500

    first commit, added spring boot files

---
+++ backend/src/test/java/org/hiredgoons/starter/ProjectStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hiredgoons.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
