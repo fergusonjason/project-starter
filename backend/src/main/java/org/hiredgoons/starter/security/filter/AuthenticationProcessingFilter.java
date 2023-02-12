c6ceeb61f56b3e339b912c0aaa0ee51dc975616d
Jason Ferguson <fergusonjason@gmail.com>
2023-02-12 16:39:02 -0500

    create deferred creation for authenticationmanager bean

--- backend/src/main/java/org/hiredgoons/starter/security/filter/AuthenticationProcessingFilter.java
+++ backend/src/main/java/org/hiredgoons/starter/security/filter/AuthenticationProcessingFilter.java
@@ -2,6 +2,7 @@ package org.hiredgoons.starter.security.filter;
 
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,9 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class AuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
 	
@@ -21,8 +25,16 @@ public class AuthenticationProcessingFilter extends AbstractAuthenticationProces
 
 	private static final RequestMatcher requestMatcher = new AntPathRequestMatcher("/login/**");
 
-	protected AuthenticationProcessingFilter(AuthenticationManager authenticationManager) {
+	public AuthenticationProcessingFilter() {
 		super(requestMatcher);
+	}
+	
+	@PostConstruct
+	public void afterPropertiesSet() {
+		
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+		AuthenticationManager authManager = context.getBean(AuthenticationManager.class);
+		setAuthenticationManager(authManager);
 	}
 
 	@Override
