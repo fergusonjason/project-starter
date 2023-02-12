d7f0d80266881645f3445f9534a98aecec34e055
Jason Ferguson <fergusonjason@gmail.com>
2023-02-12 16:40:44 -0500

    cleanup

--- backend/src/main/java/org/hiredgoons/starter/security/filter/AuthenticationProcessingFilter.java
+++ backend/src/main/java/org/hiredgoons/starter/security/filter/AuthenticationProcessingFilter.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
