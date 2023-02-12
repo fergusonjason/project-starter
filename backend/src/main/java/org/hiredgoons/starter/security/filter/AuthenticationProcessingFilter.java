8e0f6457cfd04f7f4589bffb4e6b2374a1989010
Jason Ferguson <fergusonjason@gmail.com>
2023-02-12 10:13:56 -0500

    add AuthenticationManager to filter, still not implemented

--- backend/src/main/java/org/hiredgoons/starter/security/filter/AuthenticationProcessingFilter.java
+++ backend/src/main/java/org/hiredgoons/starter/security/filter/AuthenticationProcessingFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -20,7 +21,7 @@ public class AuthenticationProcessingFilter extends AbstractAuthenticationProces
 
 	private static final RequestMatcher requestMatcher = new AntPathRequestMatcher("/login/**");
 
-	protected AuthenticationProcessingFilter() {
+	protected AuthenticationProcessingFilter(AuthenticationManager authenticationManager) {
 		super(requestMatcher);
 	}
 
